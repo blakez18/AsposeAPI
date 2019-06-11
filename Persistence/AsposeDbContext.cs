83adecf09fc3aa5e65a5427334cd5315523a58bd
Raphael Fonseca <raphael@fonseca.tech>
2019-06-11 10:09:00 -0400

    updated dbcontext

--- Persistence/AsposeDbContext.cs
+++ Persistence/AsposeDbContext.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Candidates.Models;
-
+using Positions.Models;
+using Companys.Models;
 namespace AsposeDB.persistence
 {
     public class CandidateDbContext : DbContext
@@ -11,6 +12,13 @@ namespace AsposeDB.persistence
 
     public class CompanyDbContext : DbContext
     {
-        
+        public CompanyDbContext(DbContextOptions<CompanyDbContext> options) : base(options) {}
+        public DbSet<Company> Companies {get; set;}
+    }
+
+    public class PositionDbContext : DbContext
+    {
+        public PositionDbContext(DbContextOptions<PositionDbContext> options) : base(options) {}
+        public DbSet<Position> Position {get;set;}
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -28,6 +28,7 @@ namespace AsposeAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // TODO: update connection string;
             services.AddDbContext<CandidateDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));            
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
