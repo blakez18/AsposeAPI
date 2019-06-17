2f9dfdb1d2e3c1df7a475f83131a017f5b1ca93f
Raphael Fonseca <raphael@fonseca.tech>
2019-06-17 13:38:59 -0400

    setup epplus class for testing and controller

---
+++ Controllers/EPPController.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Positions.Models;
+using System.Linq;
+using TempJson.Models;
+using Companys.Models;
+using Candidates.Models;
+using EPPService.Service;
+
+namespace EPLUSPLUSCont.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EPPController : ControllerBase
+    {
+        // Get api/aspose
+        [HttpGet]
+        public async Task<IActionResult> Post(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return Content("File not selected");
+            // Get CSV File
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", file.FileName);
+
+            using (var stream = new FileStream(path, FileMode.Create))
+                await file.CopyToAsync(stream);
+
+            
+            return null;
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            tempJson tempjson = new tempJson();
+            EPlusPlus epp = new EPlusPlus();
+
+            tempjson = GetAndConvJson(tempjson);
+            epp.EPPSetup(tempjson);
+            return "working";
+        }
+
+        public tempJson GetAndConvJson(tempJson tj)
+        {
+            // Set Path
+            const string posPath = "../Json/Position.json";
+            const string compPath = "../Json/Company.json";
+            const string canPath = "../Json/Candiate.json";
+            List<string> paths = new List<string> { posPath, compPath, canPath };
+            paths.ForEach(x =>
+            {
+                using (StreamReader r = new StreamReader(x))
+                {
+                    switch (x)
+                    {
+                        case posPath:
+                            tj.position = JsonConvert.DeserializeObject<List<Position>>(r.ReadToEnd());
+                            break;
+                        case compPath:
+                            tj.company = JsonConvert.DeserializeObject<List<Company>>(r.ReadToEnd());
+                            break;
+                        case canPath:
+                            tj.candidate = JsonConvert.DeserializeObject<List<Candidate>>(r.ReadToEnd());
+                            break;
+                    }
+                }
+            });
+            return tj;
+        }
+
+    }
+}
--- Service/EPPService.cs
+++ Service/EPPService.cs
@@ -1,7 +1,12 @@
+using System.Linq;
+using TempJson.Models;
 namespace EPPService.Service
 {
     public class EPlusPlus
     {
-        
+        public string EPPSetup(tempJson tj)
+        {
+            return null;
+        }
     }
 }
