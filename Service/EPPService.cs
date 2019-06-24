23304298f8e099bffec2f68aad42d449e303d8e8
Blake <18blakez@gmail.com>
2019-06-24 15:21:31 -0400

    enum, working on loop

--- Service/EPPService.cs
+++ Service/EPPService.cs
@@ -30,7 +30,7 @@ namespace EPPService.Service
             //FileInfo nf = new FileInfo("C:\\Users\\bzaffiro\\workrepos\\New_Data.xlsx");
             FileInfo newFile = new FileInfo(@"Test.xlsx");
             using (ExcelPackage ex = new ExcelPackage(newFile))
-            {  
+            {
                 int wsCount = ex.Workbook.Worksheets.Count() - 1;
                 while (wsCount >= 0)
                 {
@@ -41,15 +41,15 @@ namespace EPPService.Service
                 ex.Workbook.Worksheets.Add("Chart");
                 var sheet = ex.Workbook.Worksheets.Add("Data_Cells");
                 var format = new ExcelTextFormat { Delimiter = '\t', EOL = "\r" };
-               
-              
-             // Microsoft.Office.Interop.Excel.Range r = ex.Workbook.Worksheets[1].Select("A" + row.ToString(), "A" + row.ToString()).EntireRow;
-              //r.EntireRow.Insert(Microsoft.Office.Interop.Excel.XlInsertShiftDirection.xlShiftDown);
-                
-                
+
+
+                // Microsoft.Office.Interop.Excel.Range r = ex.Workbook.Worksheets[1].Select("A" + row.ToString(), "A" + row.ToString()).EntireRow;
+                //r.EntireRow.Insert(Microsoft.Office.Interop.Excel.XlInsertShiftDirection.xlShiftDown);
+
+
                 //Microsoft.Office.Interop.Excel.Range a1 = (ExcelWorksheet.Row) (ex.Workbook.Worksheets[1].InsertRow(1,1));
                 //a1.EntireRow.Insert(Microsoft.Office.Interop.Excel.XlInsertShiftDirection.xlShiftDown, 
-                    //Type.Missing );
+                //Type.Missing );
 
 
 
@@ -60,10 +60,10 @@ namespace EPPService.Service
                 else if (foj.PCCList != null)
                 {
                     ex.Workbook.Worksheets[1].Cells.LoadFromCollection(foj.PCCList.position, PrintHeaders: true); // loads only position
-                    // create header then apply data to row 0
-                  //  List<string> headerDets = new List<string>();
-                   // foj.PCCList.position.FindIndex(x => x.)
-                    //ex.Workbook.Worksheets[1].SelectedRange["A1:A1"].l("a", "b", "c", "d");
+                                                                                                                  // create header then apply data to row 0
+                                                                                                                  //  List<string> headerDets = new List<string>();
+                                                                                                                  // foj.PCCList.position.FindIndex(x => x.)
+                                                                                                                  //ex.Workbook.Worksheets[1].SelectedRange["A1:A1"].l("a", "b", "c", "d");
 
                 }
                 CreatingChart(ex);
@@ -78,31 +78,34 @@ namespace EPPService.Service
             String cellData;
 
             ExcelChart chart = ex.Workbook.Worksheets[0].Drawings.AddChart("Cool Chart", eChartType.Pie3D); //adding chart
-            int firstRowPos = ex.Workbook.Worksheets[1].Cells.Where(cell => !cell.Value.ToString().Equals("")).First().End.Row;
-            int lastRowPos = ex.Workbook.Worksheets[1].Cells.Where(cell => !cell.Value.ToString().Equals("")).Last().End.Row-1;
-            int lastColumnPos = ex.Workbook.Worksheets[1].Cells.Where(cell => !cell.Value.ToString().Equals("")).Last().End.Column-1;
+            int headerRowPos = ex.Workbook.Worksheets[1].Cells.Where(cell => !cell.Value.ToString().Equals("")).First().End.Row;
+            int lastRowPos = ex.Workbook.Worksheets[1].Cells.Where(cell => !cell.Value.ToString().Equals("")).Last().End.Row;
+            int firstColumnPos = ex.Workbook.Worksheets[1].Cells.Where(cell => !cell.Value.ToString().Equals("")).First().End.Column;
 
+            int lastColumnPos = ex.Workbook.Worksheets[1].Cells.Where(cell => !cell.Value.ToString().Equals("")).Last().End.Column;
+            
+            nsEnum.Enu AlphatoNum = new nsEnum.Enu();
 
             List<string> range = new List<string>();
             int loopCounter = 0;
-            
-            while (lastRowPos >=  loopCounter)
-            {  
-                Char r = (Char)((61) + (lastRowPos - 1));
+
+            while (lastRowPos >= loopCounter)
+            {
+                Char r = (Char)((65) + (firstColumnPos));
                 Char c = (Char)((65) + (lastColumnPos - 1));
                 range.Add(Convert.ToString(r.ToString() + (loopCounter + 1) + ":" + c.ToString() + (loopCounter + 1)));
                 //range.Add(Convert.ToString(r.ToString() + (loopCounter + 1) + ":" + c.ToString() + (loopCounter + 1)));
                 loopCounter++;
-                
-                
+
+
             }
 
             range.ForEach(x =>
             {
                 chart.Series.Add(x, "");
-                
+
             });
-            
+
 
             return ex;
         }
